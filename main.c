a59c0af37d87e744908a7da8003ffcc6b3406a5c
NGABO <56110802+ngab0@users.noreply.github.com>
2022-05-18 16:33:18 -0700

    Main File

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
-/**
+/*
  * File: main.c
- * Auth: Jack Donnily Ngabo and Emile Iyadukunze
+ * Auth: Iyadukunze Emile and Jack Donnily Ngabo
+ *       
  */
 
 #include "shell.h"
-`
+
 void sig_handler(int sig);
 int execute(char **args, char **front);
 
 /**
- * sig_handler- Prints a new prompt upon a signal
- * @sig: The signal
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
  */
 void sig_handler(int sig)
 {
-	char *new_prompt = "\n$";
+	char *new_prompt = "\n$ ";
 
 	(void)sig;
 	signal(SIGINT, sig_handler);
@@ -22,12 +23,12 @@ void sig_handler(int sig)
 }
 
 /**
- * execute- Executes a command in a child process
- * @args: An array of arguments
- * @front: A double pointer to the beginning of args
+ * execute - Executes a command in a child process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
  *
- * Return: If an error occurs - a corresponding error code
- * O/w- The exit value of the last executed command
+ * Return: If an error occurs - a corresponding error code.
+ *         O/w - The exit value of the last executed command.
  */
 int execute(char **args, char **front)
 {
@@ -80,12 +81,12 @@ int execute(char **args, char **front)
 }
 
 /**
- * main- Runs a simple UNIX command interpreter
- * @argc: The number of arguments supplied to the program
- * @argv: An array of pointers to the argument
- * Return: The return value of the last executed command
+ * main - Runs a simple UNIX command interpreter.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: The return value of the last executed command.
  */
-
 int main(int argc, char *argv[])
 {
 	int ret = 0, retn;
